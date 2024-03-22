d951be9086114b03d6f89b94837163b6b3d4e306
Pablo <pablo.magarinos@outlook.com>
2024-03-22 19:22:13 +0100

    Major methods changes
    Added aero analysis and plot functions, among other changes

--- environment/parametrization.py
+++ environment/parametrization.py
@@ -2,20 +2,22 @@ import aerosandbox as asb
 import aerosandbox.numpy as np
 import matplotlib.pyplot as plt
 import aerosandbox.tools.pretty_plots as p
+import neuralfoil as nf
 import time
 import random
 
 
 
 
-class airfoil_param:
+class airfoiltools:
     def __init__(self, n_params):
-        self.n_params = n_params
-        self.airfoil = None
+        self.n_params = n_params # Number of parameters of the airfoil 
+        self.airfoil = None # Placeholder for the airfoil object
+        self.aerodynamics = None # Placeholder for the aerodynamics of the airfoil 
 
-    def kulfan(self, lower_weights, upper_weights, leading_edge_weight, TE_thickness):
-        self.airfoil = asb.KulfanAirfoil(
-        name="My Airfoil",
+    def kulfan(self, lower_weights, upper_weights, leading_edge_weight, TE_thickness, name = ""):
+        self.airfoil = asb.KulfanAirfoil( # Create the airfoil object with the Kulfan parameterization
+        name=name,
         lower_weights=lower_weights,
         upper_weights=upper_weights,
         leading_edge_weight=leading_edge_weight,
@@ -23,24 +25,34 @@ class airfoil_param:
         )
 
     def random_kulfan(self, n_params = 50):
-        np.random.seed(int(time.time()))
+        # Randomize the weights of the airfoil
+        np.random.seed(int(time.time())) # Seed the random number generator with the current time
         
+
         lower_weights = np.random.uniform(low=-1, high=1, size=n_params) # Randomize the lower weights
 
         upper_weights = np.zeros(n_params) # Initialize the upper weights
         for i in range(len(lower_weights)): # Randomize the upper weights based on the lower weights (to not have intersections)
             upper_weights[i] = random.uniform(lower_weights[i], 1.1)
             
-
         leading_edge_weight = np.random.random()
         TE_thickness = 0 # Thickness of the trailing edge 
-        self.airfoil = asb.KulfanAirfoil(
-        name="My Airfoil",
-        lower_weights=lower_weights,
-        upper_weights=upper_weights,
-        leading_edge_weight=leading_edge_weight,
-        TE_thickness=TE_thickness
-        )
+        
+
+        # Create the airfoil
+        self.kulfan(lower_weights, upper_weights, leading_edge_weight, TE_thickness)
+
+
+    def analysis(self, angle = 0, re = 1e6, model = "xlarge"): # Analyze the airfoil and save into the aerodynamics attribute (dictionary)
+        self.aerodynamics = nf.get_aero_from_kulfan_parameters(
+            self.airfoil.kulfan_parameters, 
+            angle, re, 
+            model)
+        
+    def airfoil_plot(self):
+        fig, ax = plt.subplots(figsize=(6, 2))
+        self.airfoil.draw()
+
 
 
 
@@ -48,21 +60,25 @@ class airfoil_param:
 #### TEST ####
 
 
-
-pedro = airfoil_param(4)
+"""
+pedro = airfoiltools(4)
 
 pedro.kulfan(
-    lower_weights = -0.6 * np.ones(8),
-    upper_weights = 0.2 * np.ones(8),
-    leading_edge_weight = 0.15,
+    lower_weights = -0.2 * np.ones(8),
+    upper_weights = 0.1 * np.ones(8),
+    leading_edge_weight = 0.1,
     TE_thickness = 0
 )
-"""
+
 fig, ax = plt.subplots(figsize=(6, 2))
 pedro.airfoil.draw()
-"""
+
 pedro.random_kulfan()
 
 fig, ax = plt.subplots(figsize=(6, 2))
 pedro.airfoil.draw()
 
+pedro.analysis(0, 1e6, "xlarge")
+print(pedro.aerodynamics)
+
+"""
---
+++ testing.py
@@ -0,0 +1,21 @@
+import numpy as np
+from environment.parametrization import airfoiltools
+
+
+
+
+
+
+pedro = airfoiltools(4)
+
+pedro.kulfan(
+    lower_weights = -0.2 * np.ones(8),
+    upper_weights = 0.1 * np.ones(8),
+    leading_edge_weight = 0.1,
+    TE_thickness = 0
+)
+
+pedro.airfoil_plot()
+
+pedro.analysis(0, 1e6, "xlarge")
+print(pedro.aerodynamics)
