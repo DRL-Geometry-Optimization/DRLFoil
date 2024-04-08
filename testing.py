d08d886735c4eb9c3062ca44421c43fc9c419d41
Pablo <pablo.magarinos@outlook.com>
2024-04-09 01:04:18 +0200

    Nothing relevant

--- testing.py
+++ testing.py
@@ -1,23 +1,8 @@
-import numpy as np
 from environment.parametrization import airfoiltools
-
-
-
-
-
-
-pedro = airfoiltools()
-
-pedro.kulfan(
-    lower_weights = -0.2 * np.ones(15),
-    upper_weights = 0.1 * np.ones(9),
-    leading_edge_weight = 0.1,
-    TE_thickness = 0
-)
-
-
-pedro.airfoil_plot()
-
-pedro.modify_airfoil("up", 20, 0.3)
-
-pedro.airfoil_plot()
+from environment.reward import reward
+from environment.gym_environment import AirfoilEnv 
+"""
+s0 = [0.2*np.ones(10), -0.2*np.ones(10), 0.1]
+env = AirfoilEnv(state0=s0)
+env.state.airfoil_plot()
+"""
