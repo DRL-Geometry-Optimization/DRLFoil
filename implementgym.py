40e9e206f7c9d1c4b36ccbd970274d5ae7a6e318
Pablo <pablo.magarinos@outlook.com>
2024-04-10 20:45:01 +0200

    Update implementgym.py

--- implementgym.py
+++ implementgym.py
@@ -4,9 +4,13 @@ import environment
 import numpy as np
 
 # Create the environment
-env = gym.make('AirfoilEnv-v0', state0=[0.1*np.ones(10), 0.2*np.ones(10), 0.1])
+env = gym.make('AirfoilEnv-v0', n_params=15)
 
 # Reset the environment
-env.reset()
 
-env.step(action=[0.1*np.ones(10), 0.2*np.ones(10), 0.1])
+observation, _ = env.reset()
+
+
+observationn, _, _, _, _ = env.step(action=[0.0*np.ones(15, dtype= np.float32), 0.0*np.ones(15, dtype= np.float32), np.array([0.0], dtype=np.float32)])
+print(observationn)
+print(observation)
