34b27def4664a6ba81a6519ba5b170433107c991
Pablo <pablo.magarinos@outlook.com>
2024-04-11 13:51:45 +0200

    Update test_gym.py

--- test_gym.py
+++ test_gym.py
@@ -17,8 +17,8 @@ env.render()
 #print("ACTION:", action)
 
 for i in range(4):
-    #observationn, reward, done, _, xd = env.step(action=0.1*np.ones(30))
-    observationn, reward, done, _, xd = env.step(action=[0.12*np.ones(15), 0.02*np.ones(15), -0.05])
+    observationn, reward, done, _, xd = env.step(action=0.1*np.ones(30))
+    #observationn, reward, done, _, xd = env.step(action=[0.12*np.ones(15), 0.02*np.ones(15), -0.05])
     print("STEP:", xd)
     print("STATE:", observationn)
     print("REWARD:", reward)
