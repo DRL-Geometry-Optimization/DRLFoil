9fc26d5bacd753d2fee021fcc18f58d9003f3f21
Pablo <pablo.magarinos@outlook.com>
2024-04-14 13:11:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ from stable_baselines3.common.evaluation import evaluate_policy
 
 
 # Create the environment
-env = gym.make('AirfoilEnv-v0', n_params=10, max_steps=20)
+env = gym.make('AirfoilEnv-v0', n_params=10, max_steps=20, scale_actions = 0.4)
 # Reset the environment
 env.reset()
 
 # Instantiate the agent
-model = PPO("MlpPolicy", env, verbose=1)
+model = PPO("MlpPolicy", env, verbose=1, policy_kwargs=dict(net_arch=[64, 64]))
 # Train the agent and display a progress bar
-model.learn(total_timesteps=int(60000), progress_bar=True)
+model.learn(total_timesteps=int(100000), progress_bar=True)
 # Save the agent
-model.save("TestModel_11042024_1")
-del model  # delete trained model to demonstrate loading
+model.save("TestModel_14042024_2_scaled_04")
+#del model  # delete trained model to demonstrate loading"
