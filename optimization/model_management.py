1412ec9c5c2f0ebcbc9d4b046c6eacc59845ef61
Pablo <pablo.magarinos@outlook.com>
2024-04-14 02:53:04 +0200

    Update model_management.py

--- optimization/model_management.py
+++ optimization/model_management.py
@@ -2,5 +2,15 @@ import environment
 from environment.parametrization import airfoiltools
 
 class ModelManagement():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, model, env) -> None:
+        self.model = model
+        self.env = env
+        self.airfoil = None
+
+    def get_airfoil(self, airfoil):
+        self.airfoil = airfoiltools()
+        self.airfoil.kulfan(airfoil[0], airfoil[1], airfoil[2], airfoil[3], airfoil[4])
+
+        def predict(self):
+            pass
+    
