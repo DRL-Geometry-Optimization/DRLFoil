b764398a7a0a72e5cec7cf6bab4c60800060d342
Pablo <pablo.magarinos@outlook.com>
2024-04-16 00:34:35 +0200

    Created new reward function proposal

--- tutorials/reward_visual.py
+++ tutorials/reward_visual.py
@@ -12,7 +12,7 @@ gamma = 15 # Ancho de la campana
 
 
 # Definir los rangos de E (eficiencia) y |ΔCl| (desviación del Cl)
-E = np.linspace(0, 50, 100)  # Eficiencia aerodinámica
+E = np.linspace(0, 150, 300)  # Eficiencia aerodinámica
 delta_Cl = np.linspace(-2, 2, 50)  # Desviación de Cl
 
 # Crear una malla de valores de E y |ΔCl|
@@ -32,6 +32,31 @@ surf = ax.plot_surface(E, delta_Cl, R, cmap='viridis', edgecolor='none')
 
 # Añadir títulos y etiquetas
 ax.set_title('Función de Recompensa en 3D')
+ax.set_xlabel('Eficiencia (E)')
+ax.set_ylabel('Desviación de $C_l$ (|Δ$C_l$|)')
+ax.set_zlabel('Recompensa (R)')
+
+# Añadir una barra de colores para indicar los valores de la recompensa
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+# Mostrar la gráfica
+plt.show()
+
+
+# OTRA OPCION DE REWARD
+alpha2 = 0.1
+
+R2 = alpha2 * E * np.exp(-gamma * (delta_Cl)**2)
+
+# Crear la figura y el eje para la gráfica 3D
+fig = plt.figure(figsize=(10, 7))
+ax = fig.add_subplot(111, projection='3d')
+
+# Graficar la superficie
+surf = ax.plot_surface(E, delta_Cl, R2, cmap='viridis', edgecolor='none')
+
+# Añadir títulos y etiquetas
+ax.set_title('Función de Recompensa en 3D - Opcion 2')
 ax.set_xlabel('Eficiencia (E)')
 ax.set_ylabel('Desviación de $C_l$ (|Δ$C_l$|)')
 ax.set_zlabel('Recompensa (R)')
