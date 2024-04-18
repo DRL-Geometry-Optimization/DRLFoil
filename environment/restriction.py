ed621caa109a2e1a4ca08fbe026ab9bc26a496ec
Pablo <pablo.magarinos@outlook.com>
2024-04-18 11:56:40 +0200

    Update restriction.py

--- environment/restriction.py
+++ environment/restriction.py
@@ -6,6 +6,12 @@ class BoxRestriction:
     Box restriction class. It is used to create a space constraint in the airfoil that the optimization algorithm must respect.
     The box is defined by its center position (posx, posy), width and height.
     """
+
+    _XMIN = 0.1
+    _XMAX = 0.9
+    _YMIN = -0.4
+    _YMAX = 0.4
+
     def __init__(self, posx : float, posy : float, width : float, height : float) -> None:
         """
         Constructor of the BoxRestriction class.
@@ -43,16 +49,10 @@ class BoxRestriction:
         if not isinstance(width, (int, float)) or not isinstance(height, (int, float)):
             raise ValueError("Width and height must be numbers")
         
-        if posx > 0.9 or posx < 0.1:
-            raise ValueError("Position x must be between 0.1 and 0.9")
-        
-        if posy > 0.2 or posy < -0.2:
-            raise ValueError("Position y must be between -0.2 and 0.2")
-        
-        if self.coordinates()[0][0] < 0 or self.coordinates()[1][0] > 1:
+        if self.coordinates()[0][0] < self._XMIN or self.coordinates()[1][0] > self._XMAX:
             raise ValueError("Box is out of the environment. It must be between 0 and 1 in x axis")
         
-        if self.coordinates()[0][1] > 0.4 or self.coordinates()[2][1] < -0.4:
+        if self.coordinates()[0][1] > self._YMAX or self.coordinates()[2][1] < self._YMIN:
             raise ValueError("Box is out of the environment. It must be between -0.4 and 0.4 in y axis")        
 
 
@@ -92,7 +92,7 @@ class BoxRestriction:
     
 
 
-    def plot(self, show : bool = True) -> None:
+    def plot(self, show : bool = False) -> None:
         """
         Plots the box in the environment. If show is True, it will show the plot.
 
@@ -115,16 +115,72 @@ class BoxRestriction:
     
 
 
+    @classmethod
+    def random_box(cls, xmin : float = None, xmax : float = None, 
+                   ymin : float = None, ymax : float = None,
+                   widthmax : float = None, heightmax : float = None,
+                   y_simmetrical : bool = True) -> 'BoxRestriction':
+
+        # Set the mins and maxs with defaults values if they are None
+        if xmin is None:
+            xmin = cls._XMIN
+        if xmax is None:
+            xmax = cls._XMAX
+        if ymin is None:
+            ymin = cls._YMIN
+        if ymax is None:
+            ymax = cls._YMAX
+
+        # Set the width and height to not exceed the limits, which can be set by the user with widthmax and heightmax or the default values
+        if widthmax is None:
+            widthmax = xmax - xmin
+        else:
+            if widthmax < 0:
+                raise ValueError("Widthmax must be positive")
+            widthmax = min(widthmax, xmax - xmin) # It takes the most restrictive value
+
+        # heightmax is the same as widthmax
+        if heightmax is None:
+            heightmax = ymax - ymin
+        else:
+            if heightmax < 0:
+                raise ValueError("Heightmax must be positive")
+            heightmax = min(heightmax, ymax - ymin)
+
+        width = np.random.uniform(0.1, widthmax)
+        height = np.random.uniform(0.1, heightmax)
+
+        # After setting the width and height, it sets the position of the box according to the limits
+        posx = np.random.uniform(xmin + width/2, xmax - width/2)
+
+        if y_simmetrical:
+            posy = 0
+        else:
+            posy = np.random.uniform(ymin + height/2, ymax - height/2)
+
+        return cls(posx, posy, width, height)
+    
 
 
-def plot_boxes(*boxes):
+def plot_boxes(*boxes : BoxRestriction) -> None:
+    plt.figure(figsize=(10, 7))
+    plt.xlim(0, 1)
+    plt.ylim(-0.5, 0.5)
     for box in boxes:
+        if not isinstance(box, BoxRestriction):
+            raise TypeError("All arguments must be BoxRestriction objects")
         box.plot()
     plt.show()
 
 
-pedro = BoxRestriction(0.5, 0, 0.2, 0.2)
-pedro2 = BoxRestriction(0.3, 0, 0.2, 0.2)
-print(pedro, pedro2)
 
-print(pedro[0])
+
+
+if __name__ == "__main__":
+    for _ in range(5):
+        pedro = BoxRestriction.random_box(xmin = 0.1, xmax=0.9, ymin = -0.2, ymax= 0.2)
+        pedro.plot()
+    plt.show()
+
+    #plot_boxes(pedro)
+
