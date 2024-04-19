94d79d73086ea996af329a85536e6754df55022c
Pablo <pablo.magarinos@outlook.com>
2024-04-19 20:20:17 +0200

    Created log function

--- main_vec.py
+++ main_vec.py
@@ -2,31 +2,58 @@ import gymnasium as gym
 import environment
 import numpy as np
 import os
-
 from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
-
-
 from stable_baselines3.common.callbacks import EvalCallback
-
 from stable_baselines3.common.utils import set_random_seed
-
 from datetime import date
-
-today = date.today()
-formatted_date = today.strftime("%d%m%y")
+from recorder.create_logs import create_log
 
 # tensorboard --logdir .\logs\tensorboard_logs\FECHA\MODELO
 
+# Get the current date
+today = date.today()
+formatted_date = today.strftime("%d%m%y")
+
 ############################### MODEL NAME ########################################
-MODEL_NAME = f"{formatted_date}_ClReward_NoDelta_Wide20"
+name = "ClReward_NoDelta_Wide20_3"
 ############################### MODEL NAME ########################################
 
-LOG_DIR = f"./logs/eval_logs/{formatted_date}/{MODEL_NAME}"
-TENSORBOARD_DIR = f"./logs/tensorboard_logs/{formatted_date}/"
 
 
+############################ HYPERPARAMETERS #####################################
+n_params = 10
+max_steps = 10
+scale_actions = 0.35
+airfoil_seed = [0.1*np.ones(n_params), -0.1*np.ones(n_params), 0.0]
+delta_reward = False
+cl_reward = True
+cl_reset = None
+efficiency_param = 1
+cl_wide = 20
+
+num_cpu = 10  # Number of processes to use
+env_id = 'AirfoilEnv-v0'
+
+net_arch = [256, 256]
+total_timesteps = 100
+############################ HYPERPARAMETERS #####################################
+
+
+
+
+# Model name
+MODEL_NAME = f"{formatted_date}_{name}"
+# Model directory
+MODEL_DIR = f"./models/{formatted_date}/{MODEL_NAME}"
+# Log directory
+LOG_DIR = f"./eval_logs/{formatted_date}/{MODEL_NAME}"
+# Tensorboard directory
+#TENSORBOARD_DIR = f"./logs/tensorboard_logs/{formatted_date}/"
+
+
+# Multiprocessed environment
 def make_env(env_id: str, rank: int, seed: int = 0):
     """
     Utility function for multiprocessed env.
@@ -37,8 +64,16 @@ def make_env(env_id: str, rank: int, seed: int = 0):
     :param rank: index of the subprocess
     """
     def _init():
-        env = gym.make('AirfoilEnv-v0', n_params=10, max_steps=10, scale_actions = 0.35, airfoil_seed = [0.1*np.ones(10), -0.1*np.ones(10), 0.0],
-                       delta_reward=False, cl_reward = True, cl_reset = None, efficiency_param = 1, cl_wide = 20)
+        env = gym.make(env_id, 
+                       n_params=n_params, 
+                       max_steps=max_steps, 
+                       scale_actions = scale_actions, 
+                       airfoil_seed = airfoil_seed,
+                       delta_reward= delta_reward, 
+                       cl_reward = cl_reward, 
+                       cl_reset = cl_reset, 
+                       efficiency_param = efficiency_param, 
+                       cl_wide = cl_wide)
         env.reset(seed=seed + rank)
         return env
     set_random_seed(seed)
@@ -47,11 +82,19 @@ def make_env(env_id: str, rank: int, seed: int = 0):
 
 
 if __name__ == "__main__":
-    os.makedirs(LOG_DIR, exist_ok=True)
+    # Create log directories
+    os.makedirs(LOG_DIR, exist_ok=False)
 
 
-    env_id = 'AirfoilEnv-v0'
-    num_cpu = 10  # Number of processes to use
+    # Create log for the logs directory
+    create_log(name=MODEL_NAME, dir=LOG_DIR,
+               n_params=n_params, max_steps=max_steps, scale_actions=scale_actions,
+               airfoil_seed=airfoil_seed, delta_reward=delta_reward, cl_reward=cl_reward,
+               cl_reset=cl_reset, efficiency_param=efficiency_param, cl_wide=cl_wide,
+               num_cpu=num_cpu, net_arch=net_arch, total_timesteps=total_timesteps)
+
+    
+
 
 
     vec_env = SubprocVecEnv([make_env(env_id, i) for i in range(num_cpu)])
@@ -66,11 +109,20 @@ if __name__ == "__main__":
                                 render=False)
 
 
+
     # Instantiate the agent
-    model = PPO("MlpPolicy", vec_env, verbose=1, policy_kwargs=dict(net_arch=[256, 256]),
-                tensorboard_log=TENSORBOARD_DIR)
+    model = PPO("MlpPolicy", vec_env, verbose=1, policy_kwargs=dict(net_arch=net_arch),
+                tensorboard_log=LOG_DIR)
     # Train the agent and display a progress bar
-    model.learn(total_timesteps=int(2000000), progress_bar=True, callback=eval_callback, tb_log_name=MODEL_NAME)
+    model.learn(total_timesteps=total_timesteps, progress_bar=True, callback=eval_callback, tb_log_name="Tensorboard")
     # Save the agent
-    model.save(MODEL_NAME)
-    #del model  # delete trained model to demonstrate loading"
+    os.makedirs(MODEL_DIR, exist_ok=False)
+    model.save(f"{MODEL_DIR}/{MODEL_NAME}")
+
+    # Create log for the models directory
+    create_log(name=MODEL_NAME, dir=MODEL_DIR,
+               n_params=n_params, max_steps=max_steps, scale_actions=scale_actions,
+               airfoil_seed=airfoil_seed, delta_reward=delta_reward, cl_reward=cl_reward,
+               cl_reset=cl_reset, efficiency_param=efficiency_param, cl_wide=cl_wide,
+               num_cpu=num_cpu, net_arch=net_arch, total_timesteps=total_timesteps)
+    
---
+++ recorder/create_logs.py
@@ -0,0 +1,15 @@
+import datetime
+import os
+
+def create_log(name : str, dir : str, **vars) -> None:
+    if not os.path.exists(dir):
+        raise FileNotFoundError(f"Directory {dir} does not exist")
+    
+    with open(f"{dir}/log_{name}.txt", "w") as f:
+        f.write(f"Log created at {datetime.datetime.now()}\n\n\n")
+        for key, value in vars.items():
+            f.write(f"{key} : {value}\n")
+
+
+
+
