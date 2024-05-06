c08aebc69ad7dafbe9fd1c1bb8433f231576c943
Pablo <pablo.magarinos@outlook.com>
2024-05-06 22:39:32 +0200

    Update test_envcases.py

--- tests/test_envcases.py
+++ tests/test_envcases.py
@@ -48,15 +48,33 @@ def test_airfoil_env_obs():
     assert len(observation["cl_target"]) == 1
     assert len(observation["boxes"]) == 4*2
 
+
+    # Test case 4: 0 boxes, disabled Reynolds, cl_reset None
+    env = AirfoilEnv(n_boxes=0, reynolds=-1, cl_reset=None, max_steps=2, render_mode="no_display",
+                     airfoil_seed=[0.1*np.ones(10), -0.1*np.ones(10), 0.0])
+    env.reset()
+    observation, _, _, _, _ = env.step(np.concatenate([0.5*np.ones(10), 0.5*np.ones(10), [0.0]]))
+    assert isinstance(observation, dict)
+    assert len(observation["airfoil"]) == 10*2+1
+    assert "reynolds" not in observation
+    assert len(observation["cl_target"]) == 1
+    assert "boxes" not in observation
+
+
 def test_limits_reynolds():
     with pytest.raises(Exception):
         env = AirfoilEnv(n_boxes=1, reynolds=1e8, cl_reset=None, max_steps=2, render_mode="no_display",
                          airfoil_seed=[0.1*np.ones(10), -0.1*np.ones(10), 0.0])
         
+
+
+
 def test_limits_Cl():
     with pytest.raises(Exception):
         env = AirfoilEnv(n_boxes=1, reynolds=None, cl_reset=2, max_steps=2, render_mode="no_display",
                          airfoil_seed=[0.1*np.ones(10), -0.1*np.ones(10), 0.0])
+
+
 
 
 def test_randomness():
