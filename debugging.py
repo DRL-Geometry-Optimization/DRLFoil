a0237b726da017686148a5992fae135a93eb790b
Pablo <pablo.magarinos@outlook.com>
2024-05-09 00:34:52 +0200

    Update debugging.py

--- debugging.py
+++ debugging.py
@@ -1,9 +1,9 @@
 import drlfoil
 
-prueba = drlfoil.Optimize('twobox', 0.1, 1e7, steps = 12, logs=1)
+prueba = drlfoil.Optimize('nobox', 0.8, 1e7, steps = 12, logs=1)
 prueba.run()
 for i in range(2, 6):
-    prueba.reset(cl_target=i/5)
+    prueba.reset(cl = i/10)
     prueba.run()
 
 #prueba.save("trial")
