4b18d6dd13c7029d2e3daf71827d9086bd386b1c
Pablo <pablo.magarinos@outlook.com>
2024-05-11 01:06:17 +0200

    Added box positioning

--- debugging.py
+++ debugging.py
@@ -1,10 +1,10 @@
 import drlfoil
 
-prueba = drlfoil.Optimize('nobox', 0.8, 1e7, steps = 12, logs=1)
+prueba = drlfoil.Optimize('twobox', 0.8, 1e7, steps = 12, logs=1, boxes=[drlfoil.BoxRestriction(0.3, 0.0, 0.3, 0.1), drlfoil.BoxRestriction(0.7, .04, 0.19, 0.1)])
 prueba.run()
-for i in range(2, 6):
+"""for i in range(2, 6):
     prueba.reset(cl = i/10)
-    prueba.run()
+    prueba.run()"""
 
 #prueba.save("trial")
 
