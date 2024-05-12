5cbe60e4649ed600fbe7bf996266d8ee0f338f04
Pablo <pablo.magarinos@outlook.com>
2024-05-12 19:25:53 +0200

    Update debugging.py

--- debugging.py
+++ debugging.py
@@ -1,9 +1,10 @@
 import drlfoil
 
-prueba = drlfoil.Optimize('twobox', 0.8, 1e7, steps = 12, logs=1, boxes=[drlfoil.BoxRestriction(0.3, 0.0, 0.3, 0.1), drlfoil.BoxRestriction(0.7, .04, 0.19, 0.1)])
+prueba = drlfoil.Optimize('onebox', 0.8, 1e7, steps = 12, logs=1, boxes=[drlfoil.BoxRestriction(0.3, 0.0, 0.3, 0.1)])
 prueba.run()
-"""for i in range(2, 6):
-    prueba.reset(cl = i/10)
+prueba.analyze(plot=True)
+"""for i in range(2, 7):
+    prueba.reset(cl_target = i/5)
     prueba.run()"""
 
 #prueba.save("trial")
