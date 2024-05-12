b524adeee1f9b4fb3ea167be919e55ab2c44135a
Pablo <pablo.magarinos@outlook.com>
2024-05-12 13:19:47 +0200

    Update try_gym.py

--- try_gym.py
+++ try_gym.py
@@ -7,11 +7,11 @@ import numpy as np
 
 # Create the environment
 
-env = gym.make('AirfoilEnv-v0', n_params=10, max_steps=2, scale_actions = 0.15, airfoil_seed = [0.4*np.ones(10), -0.4*np.ones(10), 0.0],
+env = gym.make('AirfoilEnv-v0', n_params=10, max_steps=2, scale_actions = 0.15, airfoil_seed = [0.2*np.ones(10), -0.2*np.ones(10), 0.0],
                 delta_reward=False, cl_reward = True, cl_reset = None, efficiency_param = 1, cl_wide = 20,
                 render_mode="human",
                 n_boxes=1, boxes = None,
-                reynolds = 1e7)
+                reynolds = None)
 
 # Reset the environment
 
@@ -23,7 +23,7 @@ env.render()
 #action = np.concatenate([0.1*np.ones(15), -0.1*np.ones(15), 0.0])
 #print("ACTION:", action)
 
-for i in range(10):
+for i in range(5):
     observationn, reward, done, _, xd = env.step(np.concatenate([0.5*np.ones(15), 0.5*np.ones(15), [0.0]]))
     #observationn, reward, done, _, xd = env.step(action=[0.12*np.ones(15), 0.02*np.ones(15), -0.05])
     print("STEP:", xd)
